feaf0b0b648c892bedce119c1678e05ef1b1b9fd
Masterzerr <alejerry24@gmail.com>
2023-08-10 09:36:01 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+import { About, Contact, Feedback, Gallery, Header, Products, Awards, Footer } from './containers'
 import './App.css';
+import { Navbar } from './components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <About/>
+      <Products/>
+      <Feedback/>
+      <Awards/>
+      <Gallery/>
+      <Contact/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+export { default as About } from './about/About';
+export { default as Awards } from './awards/Awards';
+export { default as Contact } from './contact/Contact';
+export { default as Feedback } from './feedback/Feedback';
+export { default as Footer } from './footer/Footer';
+export { default as Gallery } from './gallery/Gallery';
+export { default as Header } from './header/Header';
+export { default as Products } from './products/Products';
